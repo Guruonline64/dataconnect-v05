50974f79138f71b087827844ba76958fa73ffb29
Guruonline64 <103130213+Guruonline64@users.noreply.github.com>
2026-09-04 09:35:27 -0700

    Set Java and Kotlin compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,4 +21,13 @@ android {
             isShrinkResources = false
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
